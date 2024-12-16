9b08c203a07b9ffa884d8735431fdd58e9175e1f
Eman12345 <eligfinkel@gmail.com>
2024-12-16 16:08:59 -0600

    Small port change

--- main.js
+++ main.js
@@ -54,6 +54,6 @@ app.post("/scrape", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
